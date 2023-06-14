002548b8d3e25f38f906958b2382ceac0b983b5e
Calvert97 <calvertlee.top@gmail.com>
2023-06-14 17:14:20 +0800

    InputControl

---
+++ 3.InputControl/Bsp/led.h
@@ -0,0 +1,15 @@
+//
+// Created by CalvertLee on 2023/5/21.
+//
+
+#ifndef INC_3_INPUTCONTROL_LED_H
+#define INC_3_INPUTCONTROL_LED_H
+
+#include "sys.h"
+
+#define LED0 PAout(1)   	//LED0
+#define LED1 PAout(5)       //LED1
+
+void LED_Init(void);
+
+#endif //INC_3_INPUTCONTROL_LED_H
---
+++ 3.InputControl/cmake-build-debug-mingw_stm32/CMakeFiles/3.InputControl.elf.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/11.HelloSTM32/3.InputControl/startup/startup_stm32f103xb.s" "E:/11.HelloSTM32/3.InputControl/cmake-build-debug-mingw_stm32/CMakeFiles/3.InputControl.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "E:/11.HelloSTM32/3.InputControl/Core/Inc"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/CMSIS/Include"
+  "E:/11.HelloSTM32/3.InputControl/System"
+  "E:/11.HelloSTM32/3.InputControl/Bsp"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/11.HelloSTM32/3.InputControl/Bsp/btn.c" "CMakeFiles/3.InputControl.elf.dir/Bsp/btn.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Bsp/btn.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Bsp/led.c" "CMakeFiles/3.InputControl.elf.dir/Bsp/led.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Bsp/led.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Core/Src/main.c" "CMakeFiles/3.InputControl.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Core/Src/main.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/3.InputControl.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Core/Src/stm32f1xx_it.c" "CMakeFiles/3.InputControl.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Core/Src/syscalls.c" "CMakeFiles/3.InputControl.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Core/Src/system_stm32f1xx.c" "CMakeFiles/3.InputControl.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "E:/11.HelloSTM32/3.InputControl/System/sys.c" "CMakeFiles/3.InputControl.elf.dir/System/sys.c.obj" "gcc" "CMakeFiles/3.InputControl.elf.dir/System/sys.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
