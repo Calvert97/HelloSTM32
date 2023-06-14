3368232992909688aa931ed754808d13b949755e
Calvert97 <calvertlee.top@gmail.com>
2023-06-14 17:06:21 +0800

    Buzzer

---
+++ 2.Buzzer/Bsp/buzzer.h
@@ -0,0 +1,15 @@
+//
+// Created by CalvertLee on 2023/5/18.
+//
+
+#ifndef INC_2_BUZZER_BUZZER_H
+#define INC_2_BUZZER_BUZZER_H
+
+#include "sys.h"
+
+#define BEEP PBout(5)   	//BEEP
+
+
+void BEEP_GPIO_Config(void);
+
+#endif //INC_2_BUZZER_BUZZER_H
---
+++ 2.Buzzer/cmake-build-debug/CMakeFiles/2.Buzzer.elf.dir/DependInfo.cmake
@@ -0,0 +1,61 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "E:/11.HelloSTM32/2.Buzzer/startup/startup_stm32f103xb.s" "E:/11.HelloSTM32/2.Buzzer/cmake-build-debug/CMakeFiles/2.Buzzer.elf.dir/startup/startup_stm32f103xb.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "E:/11.HelloSTM32/2.Buzzer/Core/Inc"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/CMSIS/Include"
+  "E:/11.HelloSTM32/2.Buzzer/Bsp"
+  "E:/11.HelloSTM32/2.Buzzer/System"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/11.HelloSTM32/2.Buzzer/Bsp/buzzer.c" "CMakeFiles/2.Buzzer.elf.dir/Bsp/buzzer.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Bsp/buzzer.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Core/Src/main.c" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/main.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Core/Src/stm32f1xx_it.c" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Core/Src/syscalls.c" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/syscalls.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Core/Src/system_stm32f1xx.c" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "E:/11.HelloSTM32/2.Buzzer/System/sys.c" "CMakeFiles/2.Buzzer.elf.dir/System/sys.c.obj" "gcc" "CMakeFiles/2.Buzzer.elf.dir/System/sys.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
